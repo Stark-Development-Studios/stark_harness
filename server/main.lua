23fe6f25e09111405f4ca0d85d5c4b5836e00ce7
Adam Biegert <theadman2092@ymail.com>
2026-06-29 09:40:05 -0600

    update(stark_harness): Bump Ox Lib Version
    Signed-off-by: Adam Biegert <theadman2092@ymail.com>

--- server/main.lua
+++ server/main.lua
@@ -1,6 +1,6 @@
 ---@diagnostic disable: lowercase-global
 
-if not lib.checkDependency('ox_lib', '3.37.0', true) then return end
+if not lib.checkDependency('ox_lib', '3.38.0', true) then return end
 
 local Config = require 'shared.config'
 
